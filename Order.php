8e23965510a7edd4399b5c58a852e10ac551ef82
Too <tootutor2014@gmail.com>
2016-01-25 12:59:28 +0700

    Fixed error on order table name in SQL

--- Order.php
+++ Order.php
@@ -14,7 +14,7 @@ class Order
   {
   	if ($userId == \TTO::getUserId() || \TTO::getRole() == 'admin') {
 			$statement = "
-	  		INSERT INTO order (userId, coin, bonus, amount, status, bankId)
+	  		INSERT INTO `order` (userId, coin, bonus, amount, status, bankId)
 	  		VALUE (:userId, :coin, :bonus, :amount, :status, :bankId)
 	  	";
 			$bind = array (
@@ -31,7 +31,7 @@ class Order
 
 			if ($row_insert > 0) { 
 		    $last_insert_id = \Db::getLastInsertId();
-		  	$statement = 'SELECT * FROM order WHERE orderId = :orderId';
+		  	$statement = 'SELECT * FROM `order` WHERE orderId = :orderId';
 		  	$bind = array('orderId' => $last_insert_id);
 				return \Db::getResult($statement, $bind);
 			} else {
@@ -50,7 +50,7 @@ class Order
   	if (\TTO::getRole() == 'admin') {
 	  	$statement = '
 	  		SELECT O.*, U.nickname 
-	  		  FROM order AS O
+	  		  FROM `order` AS O
 	  		 INNER JOIN user AS U
 	  		    ON UC.userId = U.userId
 	  		 ORDER BY status DESC
@@ -67,7 +67,7 @@ class Order
   protected function getAllUserOrder($userId)
   {
   	if ($userId == \TTO::getUserId() || \TTO::getRole() == 'admin') {
-	  	$statement = 'SELECT * FROM coin_order WHERE userId = :userId ORDER BY status DESC';
+	  	$statement = 'SELECT * FROM `order` WHERE userId = :userId ORDER BY status DESC';
 	  	$bind = array('userId' => $userId);
 			return \Db::getResult($statement, $bind);
   	} else {
@@ -82,7 +82,7 @@ class Order
   {
   	if ($userId == \TTO::getUserId() || \TTO::getRole() == 'admin') {
 	  	$statement = '
-	  		UPDATE order SET 
+	  		UPDATE `order` SET 
 	  			status         = :status, 
 	  			bankId         = :bankId, 
 	  			transferAmount = :transferAmount,
@@ -110,7 +110,7 @@ class Order
   protected function deleteOrder($orderId, $userId)
   {
   	if ($userId == \TTO::getUserId() || \TTO::getRole() == 'admin') {
-			$statement = 'DELETE order WHERE orderId = :orderId';
+			$statement = 'DELETE `order` WHERE orderId = :orderId';
 			$bind = array('orderId' => $orderId);
 			$count = \Db::execute($statement, $bind);
 
@@ -132,7 +132,7 @@ class Order
   protected function postApproveOrder($orderId)
   {
   	if (\TTO::getRole() == 'admin') {
-	  	$statement = 'UPDATE order SET status = :status WHERE orderId = :orderId';
+	  	$statement = 'UPDATE `order` SET status = :status WHERE orderId = :orderId';
 	  	$bind = array('orderId' => $orderId, 'status' => 'approve');
 			$count = \Db::execute($statement, $bind);
 
@@ -140,7 +140,7 @@ class Order
 			\TTOMail::createAndSend(ADMINEMAIL, \TTO::getUserEmail($userId), 'Admin have approved your order', 'Please check on the system');
 
 			if ($count > 0) {
-				$statement = 'SELECT coin + bonus FROM coin_order WHERE orderId = :orderId';
+				$statement = 'SELECT coin + bonus FROM `order` WHERE orderId = :orderId';
 				$bind = array('orderId' => $orderId);
 				$coin = \Db::getValue($statement, $bind);
 
