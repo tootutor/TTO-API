c2dbb15cb1d9be5fcf1bc9ca546ad2546179f230
Too <tootutor2014@gmail.com>
2016-02-05 18:22:38 +0700

    Task Item API

--- Task.php
+++ Task.php
@@ -10,14 +10,20 @@ class Task
   /**
    * @url POST
    */ 
-	protected function postNewTask($code, $content, $taskTypeId, array $allItem) 
+	protected function postNewTask($code, $content, $rank, $taskTypeId, $sectionId) 
 	{
   	if (\TTO::getRole() == 'admin') {
 			$statement = '
-				INSERT INTO task (code, content, taskTypeId)
+				INSERT INTO task (code, content, rank, taskTypeId, sectionId)
 				VALUES (:code, :content, :taskTypeId)
 			';
-			$bind = array('code' => $code, 'content' => $content, 'taskTypeId' => $taskTypeId);
+			$bind = array(
+        'code' => $code, 
+        'content' => $content, 
+        'rank' => $rank, 
+        'taskTypeId' => $taskTypeId
+        'sectionId' => $sectionId
+      );
 			$row_insert = \Db::execute($statement, $bind);
 			$taskId = \Db::getLastInsertId();
 
@@ -30,309 +36,73 @@ class Task
 	}
 
   /**
-   * @url GET allitem
+   * @url GET
    */ 
-	protected function getAllItem() 
+	protected function getAllTask($sectionId) 
 	{
-  	$statement = 'SELECT * FROM item';
-		return \Db::getResult($statement);
+  	if (\TTO::getRole() == 'admin') {
+      $statement = 'SELECT * FROM task WHERE sectionId = :sectionId';
+			$bind = array('sectionId' => $sectionId);
+      
+      return \Db::getResult($statement);
+  	} else {
+  		throw new RestException(401, 'No Authorize or Invalid request !!!');
+  	}
 	}
 
   /**
-   * @url GET allnewitem
+   * @url GET /user/{userId}
    */ 
-	protected function getNewAllItem() 
-	{
-  	$statement = '
-  		SELECT * FROM item AS I 
-  		WHERE NOT EXISTS (SELECT 1 FROM course_item AS CI
-  		                  WHERE CI.itemId = I.itemId)
-  	';
-		return \Db::getResult($statement);
-	}
-
-  /**
-   * @url GET user/{userId}
-   */ 
-	protected function getAllUserItem($userId, $sectionId) 
+	protected function getAllUserTask($userId, $sectionId) 
 	{
     if ($userId == \TTO::getUserId() || \TTO::getRole() == 'admin') {
       $statement = '
-        SELECT I.*, IG.name AS itemGroup, IG.theme
-        FROM item AS I
-        INNER JOIN item_group AS IG
-        ON IG.itemGroupId = I.itemGroupId
-        WHERE I.sectionId = :sectionId
+        SELECT T.*, UT.userId, UT.status, UT.point
+        FROM task AS T
+        LEFT OUTER JOIN user_task AS UT
+        ON UT.taskId = T.taskId
+        AND UT.userId = :userId
+        WHERE T.sectionId = :sectionId
       ';
-      $bind = array('sectionId' => $sectionId);
-      return \Db::getResult($statement, $bind);
-    } else {
-      throw new RestException(401, 'No Authorize or Invalid request !!!');
-    }
-		return \Db::getResult($statement);
+			$bind = array(
+        'userId' => $userId
+        'sectionId' => $sectionId
+      );
+      return \Db::getResult($statement);
+  	} else {
+  		throw new RestException(401, 'No Authorize or Invalid request !!!');
+  	}
 	}
   
-  
   /**
-   * @url GET item/{itemId}
+   * @url PUT /{taskId}
+   * @url PUT /{taskId}/user/{userId}
    */ 
-	protected function getItem($itemId) 
-	{
-		$statement = 'SELECT * FROM item WHERE itemId = :itemId';
-		$bind = array('itemId' => $itemId);
-		$response = \Db::getRow($statement, $bind);
-		$response += array('allItemDetail' => array());
-
-		$statement = 'SELECT * FROM item_detail WHERE itemId = :itemId ORDER BY seq';
-		$bind = array('itemId' => $itemId);
-		$allItemDetail = \Db::getResult($statement, $bind);
-
-		foreach ($allItemDetail as $itemDetail) {
-			if ($itemDetail['itemTypeId'] == 1) {
-				$statement = 'SELECT * FROM item_radio WHERE itemDetailId = :itemDetailId';
-				$bind = array('itemDetailId' => $itemDetail['itemDetailId']);
-				$allItemRadio = \Db::getResult($statement, $bind);
-				$itemDetail += array('allItemRadio' => $allItemRadio);
-			}
-			if ($itemDetail['itemTypeId'] == 3) {
-				$statement = 'SELECT * FROM item_select WHERE itemDetailId = :itemDetailId';
-				$bind = array('itemDetailId' => $itemDetail['itemDetailId']);
-				$allItemSelect = \Db::getResult($statement, $bind);
-				$itemDetail += array('allItemSelect' => $allItemSelect);
-			}
-			if ($itemDetail['itemTypeId'] == 4) {
-				$statement = 'SELECT * FROM item_input WHERE itemDetailId = :itemDetailId';
-				$bind = array('itemDetailId' => $itemDetail['itemDetailId']);
-				$allItemInput = \Db::getResult($statement, $bind);
-				$itemDetail += array('allItemInput' => $allItemInput);
-			}
-			$response['allItemDetail'][] = $itemDetail;
-		}
-
-		return $response;
-	}
-
-  /**
-   * @url POST updateitem
-   */ 
-	protected function postUpdateItem($itemId, $code, $content, $itemGroupId) 
+	protected function putUpdateTask($taskId, $code, $content, $rank, $taskTypeId, $sectionId) 
 	{
   	if (\TTO::getRole() == 'admin') {
 			$statement = '
-				UPDATE item 
+				UPDATE task 
 				SET code        = :code, 
 				    content     = :content,
-				    itemGroupId = :itemGroupId
-				WHERE itemId = :itemId
+				    rank        = :rank,
+				    itemTypeId  = :itemTypeId,
+				    sectionId   = :sectionId
+				WHERE taskId = :taskId
 			';
 	  	$bind = array(
-	  		'itemId'       => $itemId,
+	  		'taskId'       => $taskId,
 	  		'code'         => $code,
 	  		'content'      => $content,
-	  		'itemGroupId'  => $itemGroupId
-	  	);
-			$row_update = \Db::execute($statement, $bind);
-
-	  	$response = new \stdClass();
-			$response->row_update = $row_update;
-			return $response;
-  	} else {
-  		throw new RestException(401, 'No Authorize or Invalid request !!!');
-  	}
-	}
-
-  /**
-   * @url GET allitemtype
-   */ 
-	protected function getAllItemType() 
-	{
-  	$statement = 'SELECT * FROM item_type';
-		$response = \Db::getResult($statement);
-		
-		return $response;
-	}
-
-  /**
-   * @url GET getitemdetail/{itemDetailId}
-   */ 
-	protected function getItemDetail($itemDetailId) 
-	{
-  	$statement = 'SELECT * FROM item_detail WHERE itemDetailId = :itemDetailId';
-  	$bind = array('itemDetailId' => $itemDetailId);
-		$response = \Db::getResult($statement, $bind);
-		
-		return $response;
-	}
-
-  /**
-   * @url POST newitemdetail
-   */ 
-	protected function postNewItemDetail($itemId, $seq, $itemTypeId, $code, $content, $isAction, $showOption) 
-	{
-  	if (\TTO::getRole() == 'admin') {
-			$statement = '
-				INSERT INTO item_detail (itemId, seq, itemTypeId, code, content, isAction, showOption)
-				VALUES (:itemId, :seq, :itemTypeId, :code, :content, :isAction, :showOption)
-			';
-	  	$bind = array(
-	  		'itemId'      => $itemId,
-	  		'seq'         => $seq,
-	  		'itemTypeId'  => $itemTypeId,
-	  		'code'        => $code,
-	  		'content'     => $content,
-	  		'isAction'    => $isAction,
-	  		'showOption'  => $showOption
-	  	);
-			$row_insert = \Db::execute($statement, $bind);
-			$itemDetailId = \Db::getLastInsertId();
-
-	  	$response = new \stdClass();
-			$response->itemDetailId = $itemDetailId;
-			return $response;
-  	} else {
-  		throw new RestException(401, 'No Authorize or Invalid request !!!');
-  	}
-	}
-
-  /**
-   * @url POST updateitemdetail
-   */ 
-	protected function postUpdateItemDetail($itemDetailId, $itemId, $seq, $itemTypeId, $code, $isAction, $showOption, $content="") 
-	{
-  	if (\TTO::getRole() == 'admin') {
-			$statement = '
-				UPDATE item_detail 
-				SET itemId     = :itemId, 
-				    seq        = :seq, 
-				    itemTypeId = :itemTypeId,
-				    code       = :code, 
-				    content    = :content, 
-				    isAction   = :isAction,
-				    showOption = :showOption
-				WHERE itemDetailId = :itemDetailId
-			';
-	  	$bind = array(
-	  		'itemDetailId' => $itemDetailId,
-	  		'itemId'       => $itemId,
-	  		'seq'          => $seq,
+	  		'rank'         => $content,
 	  		'itemTypeId'   => $itemTypeId,
-	  		'code'         => $code,
-	  		'content'      => $content,
-	  		'isAction'     => $isAction,
-	  		'showOption'   => $showOption
+	  		'sectionId'    => $sectionId
 	  	);
-			$row_update = \Db::execute($statement, $bind);
-
-	  	$response = new \stdClass();
-			$response->row_update = $row_update;
-			return $response;
-  	} else {
-  		throw new RestException(401, 'No Authorize or Invalid request !!!');
-  	}
-
-	}
-
-  /**
-   * @url POST updateitemradio
-   */ 
-	protected function postUpdateItemRadio($itemRadioId, $content, $isAnswer, $point) 
-	{
-  	if (\TTO::getRole() == 'admin') {
-			$statement = '
-				UPDATE item_radio 
-				SET content    = :content, 
-				    isAnswer   = :isAnswer,
-				    point      = :point
-				WHERE itemRadioId = :itemRadioId
-			';
-	  	$bind = array(
-	  		'itemRadioId' => $itemRadioId,
-	  		'content'     => $content,
-	  		'isAnswer'    => $isAnswer,
-	  		'point'       => $point
-	  	);
-			$row_update = \Db::execute($statement, $bind);
-
-	  	$response = new \stdClass();
-			$response->row_update = $row_update;
-			return $response;
-  	} else {
-  		throw new RestException(401, 'No Authorize or Invalid request !!!');
-  	}
-
-	}
-
-  /**
-   * @url POST updateitemselect
-   */ 
-	protected function postUpdateItemSelect($itemSelectId, $content, $isAnswer, $point) 
-	{
-  	if (\TTO::getRole() == 'admin') {
-			$statement = '
-				UPDATE item_select 
-				SET content    = :content, 
-				    isAnswer   = :isAnswer,
-				    point      = :point
-				WHERE itemSelectId = :itemSelectId
-			';
-	  	$bind = array(
-	  		'itemSelectId' => $itemSelectId,
-	  		'content'      => $content,
-	  		'isAnswer'     => $isAnswer,
-	  		'point'        => $point
-	  	);
-			$row_update = \Db::execute($statement, $bind);
-
-	  	$response = new \stdClass();
-			$response->row_update = $row_update;
-			return $response;
+			\Db::execute($statement, $bind);
+			return;
   	} else {
   		throw new RestException(401, 'No Authorize or Invalid request !!!');
   	}
 	}
-	
-  /**
-   * @url POST updateiteminput
-   */ 
-	protected function postUpdateItemInput($itemInputId, $question, $answer, $answerType, $point) 
-	{
-  	if (\TTO::getRole() == 'admin') {
-			$statement = '
-				UPDATE item_input
-				SET question   = :question, 
-				    answer     = :answer,
-				    answerType = :answerType,
-				    point      = :point
-				WHERE itemInputId = :itemInputId
-			';
-	  	$bind = array(
-	  		'itemInputId' => $itemInputId,
-	  		'question'    => $question,
-	  		'answer'      => $answer,
-	  		'answerType'  => $answerType,
-	  		'point'       => $point
-	  	);
-			$row_update = \Db::execute($statement, $bind);
-
-	  	$response = new \stdClass();
-			$response->row_update = $row_update;
-			return $response;
-  	} else {
-  		throw new RestException(401, 'No Authorize or Invalid request !!!');
-  	}
-
-	}
-
-  /**
-   * @url GET allitemgroup
-   */ 
-	protected function getAllItemGroup() 
-	{
-  	$statement = 'SELECT * FROM item_group';
-		$response = \Db::getResult($statement);
-		
-		return $response;
-	}
-
 	
 }
