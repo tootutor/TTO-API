04c4d986dd6fba5fc038943f31e61aecfaa7d7a9
Too <tootutor2014@gmail.com>
2016-02-23 13:43:37 +0700

    update conversion for all item types

--- Conversion.php
+++ Conversion.php
@@ -8,34 +8,63 @@ class Conversion
 	 */
 
   /**
-   * @url GET /{itemTypeId}
+   * @url GET /
    */ 
-	protected function getConvertItem($itemTypeId) 
+	protected function getConvertItem() 
 	{
   	if (\TTO::getRole() == 'admin') {
       $response = new \stdClass();
       
-      $statement = 'SELECT itemId, code, content FROM item WHERE itemTypeId = :itemTypeId';
-      $bind = array('itemTypeId' => $itemTypeId);
-      $allItem = \Db::getResult($statement, $bind);
+      $statement = 'SELECT itemId, code, content, itemTypeId FROM item';
+      //$bind = array('itemTypeId' => $itemTypeId);
+      $allItem = \Db::getResult($statement);
 
       foreach ($allItem as &$item) {
         $newItem = new \stdClass();
-        $newItem->question = $item['content'];
-        $statement = 'SELECT content, isAnswer, point FROM item_radio WHERE itemId = :itemId';
-        $bind = array('itemId' => $item['itemId']);
-        $newItem->allRadio = \Db::getResult($statement, $bind);
         
-        foreach ($newItem->allRadio as &$radio) {
-          if ($radio['isAnswer']) {
-            $radio['isAnswer'] = true;
-          } else {
-            $radio['isAnswer'] = false;
-          }
+        switch ($item['itemTypeId']) {
+          case 1:
+            $newItem->question = $item['content'];
+            $statement = 'SELECT content, isAnswer, point FROM item_radio WHERE itemId = :itemId';
+            $bind = array('itemId' => $item['itemId']);
+            $newItem->allRadio = \Db::getResult($statement, $bind);
+            
+            foreach ($newItem->allRadio as &$radio) {
+              if ($radio['isAnswer']) {
+                $radio['isAnswer'] = true;
+              } else {
+                $radio['isAnswer'] = false;
+              }
+            }
+            break;
+          case 3:
+            $newItem->question = $item['content'];
+            $statement = 'SELECT content, isAnswer, point FROM item_select WHERE itemId = :itemId';
+            $bind = array('itemId' => $item['itemId']);
+            $newItem->allSelect = \Db::getResult($statement, $bind);
+            
+            foreach ($newItem->allSelect as &$select) {
+              if ($select['isAnswer']) {
+                $select['isAnswer'] = true;
+              } else {
+                $select['isAnswer'] = false;
+              }
+            }
+            break;
+          case 4:
+            $newItem->question = $item['content'];
+            $statement = 'SELECT question, answer, answerType, point FROM item_input WHERE itemId = :itemId';
+            $bind = array('itemId' => $item['itemId']);
+            $newItem->allInput = \Db::getResult($statement, $bind);
+            break;
+          default:
+            break;
         }
-        
-        $content2 = json_encode($newItem, JSON_UNESCAPED_UNICODE);
-        
+        if ($item['itemTypeId'] == 1 || $item['itemTypeId'] == 3 || $item['itemTypeId'] == 4) {
+          $content2 = json_encode($newItem, JSON_UNESCAPED_UNICODE);
+        } else {
+          $content2 = $item['content'];
+        }
         $statement = '
           UPDATE item
           SET content2 = :content2
@@ -46,9 +75,6 @@ class Conversion
         
         $item['content2'] = $content2;
       }
-      //$statement = 'SELECT itemId, code, content2 FROM item WHERE itemTypeId = :itemTypeId';
-      //$bind = array('itemTypeId' => $itemTypeId);
-			//return \Db::getResult($statement, $bind);
       return $allItem;
   	} else {
   		throw new RestException(401, 'No Authorize or Invalid request !!!');
